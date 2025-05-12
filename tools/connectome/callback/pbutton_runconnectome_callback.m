2e51a52690b465a96eb426b303eb44fc40e48c66
Andre Peres <peres.asc@gmail.com>
2025-05-12 19:56:50 +0100

    add high-pass and low-passto opts variable in pbutton_runconnectome_callback

--- tools/connectome/callback/pbutton_runconnectome_callback.m
+++ tools/connectome/callback/pbutton_runconnectome_callback.m
@@ -22,6 +22,8 @@ opts.rsave = get(handles.tools.connectome.checkbox_rsave,'value');
 opts.psave = get(handles.tools.connectome.checkbox_psave,'value');
 opts.zsave = get(handles.tools.connectome.checkbox_zsave,'value');
 opts.ftsave = get(handles.tools.connectome.checkbox_ftsave,'value');
+opts.higpass = get(handles.tools.connectome.edit_highpass,'string');
+opts.lowpass = get(handles.tools.connectome.edit_lowpass,'string');
 
 if ~(opts.rsave || opts.zsave || opts.ftsave)
     he = errordlg('Please, select at least one type of output!');
