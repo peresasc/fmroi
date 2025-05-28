5d6b8f2ee0c9a33392e3079f0a348222440c7681
Andre Peres <peres.asc@gmail.com>
2025-05-28 13:13:59 +0100

    Fix loading of confounds for multiple subjects in runconnectome function

--- tools/connectome/method/runconnectome.m
+++ tools/connectome/method/runconnectome.m
@@ -107,129 +107,6 @@ if isempty(tspath) || isempty(outdir)
     return
 end
 
-% %--------------------------------------------------------------------------
-% % Get the parameters for filtering
-% if ~isempty(opts.filter)
-%     if isfield(opts.filter,'tr')
-%         %--------------------------------------------------------------------------
-%         % Convert opts.filter.tr to double
-%         if isnumeric(opts.filter.tr) && isscalar(opts.filter.tr)
-%             tr = double(opts.filter.tr);
-%             disp(['TR equal to ',num2str(tr),' s.']);
-% 
-%         elseif ischar(opts.filter.tr)
-%             tr = str2double(opts.filter.tr);
-% 
-%             if isnan(tr)
-%                 tr = 1;
-%                 disp(['Warning: Invalid TR value format. ',...
-%                     'TR set to ',num2str(tr),' s.']);
-%             else
-%                 disp(['TR equal to ',num2str(tr),' s.']);
-%             end
-% 
-%         else
-%             tr = 1;
-%             disp(['Warning: Invalid TR value format. ',...
-%                 'TR set to ',num2str(tr),' s.']);
-%         end
-%     else
-%         tr = 1;
-%         disp(['Warning: Invalid TR value format. ',...
-%             'TR set to ',num2str(tr),' s.']);
-%     end
-% 
-%     %--------------------------------------------------------------------------
-%     % Convert opts.filter.highpass to double
-%     if isfield(opts.filter,'highpass')
-%         if isnumeric(opts.filter.highpass) && isscalar(opts.filter.highpass)
-%             hp = double(opts.filter.highpass);
-%             disp(['High-pass filter of ',num2str(hp),' Hz will be applied.']);
-% 
-%         elseif ischar(opts.filter.highpass)
-%             hp = str2double(opts.filter.highpass);
-% 
-%             if strcmpi(opts.filter.highpass,'none')
-%                 disp('Warning: High-pass filter will not be applied.');
-%             elseif isnan(hp)
-%                 disp(['Warning: Invalid High-pass filter value format. ',...
-%                     'No filter will be applied.']);
-%             else
-%                 disp(['High-pass filter of ',num2str(hp),' Hz will be applied.']);
-%             end
-% 
-%         else
-%             hp = nan;
-%             disp(['Warning: Invalid High-pass filter value format. ',...
-%                 'No High-pass filter will be applied.']);
-%         end
-%     else
-%         hp = nan;
-%         disp(['Warning: Invalid High-pass filter value format. ',...
-%             'No High-pass filter will be applied.']);
-%     end
-% 
-%     %--------------------------------------------------------------------------
-%     % Convert opts.filter.lowpass to double
-%     if isfield(opts.filter,'lowpass')
-%         if isnumeric(opts.filter.lowpass) && isscalar(opts.filter.lowpass)
-%             lp = double(opts.filter.lowpass);
-%             disp(['Low-pass filter of ',num2str(lp),' Hz will be applied.']);
-% 
-%         elseif ischar(opts.filter.lowpass)
-%             lp = str2double(opts.filter.lowpass);
-% 
-%             if strcmpi(opts.filter.lowpass,'none')
-%                 disp('Warning: Low-pass filter will not be applied.');
-%             elseif isnan(lp)
-%                 disp(['Warning: Invalid Low-pass filter value format. ',...
-%                     'No filter will be applied.']);
-%             else
-%                 disp(['Low-pass filter of ',num2str(lp),' Hz will be applied.']);
-%             end
-% 
-%         else
-%             lp = nan;
-%             disp(['Warning: Invalid Low-pass filter value format. ',...
-%                 'No Low-pass filter will be applied.']);
-%         end
-%     else
-%         lp = nan;
-%         disp(['Warning: Invalid Low-pass filter value format. ',...
-%             'No Low-pass filter will be applied.']);
-%     end
-% 
-%     %--------------------------------------------------------------------------
-%     % Convert opts.filter.order to double
-%     if isfield(opts.filter,'order')        
-%         if isnumeric(opts.filter.order) && isscalar(opts.filter.order)
-%             order = double(opts.filter.order);
-%             disp(['Filter order equal to ',num2str(order),'.']);
-% 
-%         elseif ischar(opts.filter.order)
-%             order = str2double(opts.filter.order);
-% 
-%             if isnan(order)
-%                 order = 1;
-%                 disp(['Warning: Invalid order value format. ',...
-%                     'Order set to ',num2str(order),'.']);
-%             else
-%                 disp(['Order equal to ',num2str(order),'.']);
-%             end
-% 
-%         else
-%             order = 1;
-%             disp(['Warning: Invalid order value format. ',...
-%                     'Order set to ',num2str(order),'.']);
-%         end
-%     else
-%         order = 1;
-%         disp(['Warning: Invalid order value format. ',...
-%                     'Order set to ',num2str(order),'.']);
-%     end
-% end
-
-
 %--------------------------------------------------------------------------
 % Check if oudir exists, otherwise it creates outdir
 if ~isfolder(outdir)
@@ -264,6 +141,15 @@ else
         auxtspath{ss} = tspath(pathsep(ss)+1:pathsep(ss+1)-1); % convert paths string to cell array
     end
     tspath = auxtspath;
+end
+
+if isfield(opts,'regout') && isfield(opts.regout,'conf') &&...
+        iscell(opts.regout.conf) && length(opts.regout.conf)~=length(tspath)
+    he = errordlg(['The number of confound files must match the number of subject time series.', ...
+                   newline, 'Please check the field "opts.regout.conf".'], ...
+                   'Mismatched Input');
+    uiwait(he)
+    return
 end
 
 %--------------------------------------------------------------------------
@@ -336,8 +222,13 @@ for k = 1:length(tspath)
         % CLEANING DATA
         auxts = ts;
         if isfield(opts,'regout') && ~isempty(opts.regout)
-            auxts = preproc_regout(auxts,opts.regout.conf,...
-                opts.regout.selconf,opts.regout.demean);
+            if iscell(opts.regout.conf)
+                auxts = preproc_regout(auxts,opts.regout.conf{k},...
+                    opts.regout.selconf,opts.regout.demean);
+            else
+                auxts = preproc_regout(auxts,opts.regout.conf,...
+                    opts.regout.selconf,opts.regout.demean);
+            end
         end
 
         if isfield(opts,'filter') && ~isempty(opts.filter)
