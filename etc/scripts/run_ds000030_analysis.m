1be9c5d9e7a57b46ee860f5f26faef3abb32c90d
peresasc <peres.asc@gmail.com>
2025-05-29 17:17:09 +0100

    create main script for analysing ds00030

--- etc/scripts/run_ds000030_analysis.m
+++ etc/scripts/run_ds000030_analysis.m
@@ -1,5 +1,11 @@
 %% run_ds000030_analysis
 
 srcpath = '/media/andre/data8t/ds000030/funcpath.txt';
-maskpath = ''; 
+maskpath = '';
+outdir = '/media/andre/data8t/ds000030/connectomes';
+optspath = '/media/andre/data8t/ds000030/opts.mat';
+auxopts = load(optspath);
+varname = fieldnames(auxopts);
+opts = auxopts.(varname{1});
+
 runapplymask(srcpath,maskpath,outdir,opts)
